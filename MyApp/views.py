587b3559ac0724d645869540d41b3938029a5ffe
michi <michiyo.knst@gmail.com>
2023-01-17 15:37:42 -0500

    Initial commit

---
+++ MyApp/views.py
@@ -0,0 +1,13 @@
+import datetime
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    data = dict()
+    import datetime
+    time = datetime.datetime.now()
+    data["time_of_day"] = time
+    print(time)
+    return render(request, "home.html", context=data)

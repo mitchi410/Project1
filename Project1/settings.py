7ee9a5d4849486e589936fbbd2dcc0c28681b780
michi <michiyo.knst@gmail.com>
2023-01-17 15:50:42 -0500

    Fixed an issue in the databases settings

--- Project1/settings.py
+++ Project1/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'Project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+ 'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/hj2203-webapps1',
+                                   conn_max_age=600)}
+
 """
- DATABASES = {
- 'default': dj_database_url.config(default=‘postgresql://postgres:postgres@localhost:5432/hj2203-webapps1',
- conn_max_age=600)}
+DATABASES = {
+    'default': {
          'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
